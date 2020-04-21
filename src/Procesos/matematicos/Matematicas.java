0fad997fc0b0df7171bcbc18bdae1404e76298ed
luis3 <luis3@LAPTOP-I5BIBL0E>
2020-04-20 20:45:39 -0500

    practica 5

---
+++ src/Procesos/matematicos/Matematicas.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Procesos.matematicos;
+import static java.lang.Math.PI;
+/**
+ */
+public class Matematicas {
+    public double pi(){
+        return Math.PI;
+    }
+    public int suma(int a,int b){
+        return (a+b);
+    }
+    
+}
---
+++ src/Procesos/matematicos/datos/informacion.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Procesos.matematicos.datos;
+
+/**
+ */
+public class informacion {
+
+public String saludo(String cadena){
+    return "bienvenido" + cadena;
+}   
+}
